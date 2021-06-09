ec5c898a8445ec5f3d52579e8f6e8289e06698f9
WIBR <waffeu12092001@mail.com>
2021-06-09 16:41:19 +0200

    Debut de la page d'inscription et du stepper

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import {NotfoundComponent} from "./notfound/notfound.component";
+import {NotfoundComponent} from './notfound/notfound.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/inscription/inscription.module.ts
+++ src/app/inscription/inscription.module.ts
@@ -4,6 +4,10 @@ import { InscriptionComponent } from './inscription/inscription.component';
 import {InscriptionRoutingModule} from "./inscription-routing.module";
 import {InscriptionShellComponent} from "./inscription-shell/inscription-shell.component";
 import {HeaderComponent} from "../header/header.component";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {StepperOrientation} from "@angular/material/stepper/testing";
 
 
 
@@ -15,7 +19,9 @@ import {HeaderComponent} from "../header/header.component";
   ],
   imports: [
     CommonModule,
-    InscriptionRoutingModule
+    InscriptionRoutingModule,
+    MatStepperModule,
+    MatButtonModule,MatIconModule,StepperOrientation
   ],
   exports: [
     HeaderComponent
