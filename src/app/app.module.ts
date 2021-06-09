6d4a81d11d3089a5c104c0e133cd2a48d7e1cfba
WIBR <waffeu12092001@mail.com>
2021-06-09 12:27:19 +0200

    Ajout du header sur la page d'inscription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import { InscriptionShellComponent } from './inscription/inscription-shell/inscription-shell.component';
-import { HelpShellComponent } from './help/help-shell/help-shell.component';
-import { HelpComponent } from './help/help/help.component';
-import { HeaderComponent } from './header/header.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
     NotfoundComponent,
   ],
   imports: [
--- src/app/inscription/inscription.module.ts
+++ src/app/inscription/inscription.module.ts
@@ -3,17 +3,22 @@ import { CommonModule } from '@angular/common';
 import { InscriptionComponent } from './inscription/inscription.component';
 import {InscriptionRoutingModule} from "./inscription-routing.module";
 import {InscriptionShellComponent} from "./inscription-shell/inscription-shell.component";
+import {HeaderComponent} from "../header/header.component";
 
 
 
 @NgModule({
   declarations: [
     InscriptionComponent,
-    InscriptionShellComponent
+    InscriptionShellComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     InscriptionRoutingModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class InscriptionModule { }
